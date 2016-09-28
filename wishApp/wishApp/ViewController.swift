713076a4a9441d0fe94c09ce91926fb247bfa098
-kenithcai <-kenithcai@gmail.com>
2016-09-28 10:54:58 +0800

    创建工程

---
+++ wishApp/wishApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wishApp
+//
+//  Created by KenithCai on 16/9/28.
+//  Copyright © 2016年 KenithCai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
